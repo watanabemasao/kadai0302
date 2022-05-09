a01555319c39b55d5ccff0d30336da8934674cec
渡辺正雄 <watanabemasao@watanabemasaonoMacBook-Pro.local>
2022-05-09 23:35:49 +0900

    gitignoreの修正

---
+++ src/main/java/hellodemo/HelloController.java
@@ -0,0 +1,15 @@
+package hellodemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-sample")
+    public String hello(){
+
+        return "hello.html";
+    }
+
+}
